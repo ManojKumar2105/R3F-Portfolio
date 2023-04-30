f5519d4e9322d85242f2750f20beefb982f7573a
MANOJ KUMAR <100888762+Manojkumar1030@users.noreply.github.com>
2023-04-30 18:24:36 +0530

    Initial commit

---
+++ src/Experience.js
@@ -0,0 +1,60 @@
+import { Html,Environment, Float, OrbitControls, PresentationControls, useGLTF, Text } from '@react-three/drei'
+
+export default function Experience()
+{
+
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    return <>
+
+        <color args={['#695b5b']} attach="background" />
+        <Environment preset='city' />
+        
+        {/* <OrbitControls makeDefault /> */}
+        <PresentationControls 
+            global
+            rotation={[0.13,0.1,0]}
+            polar={[-0.4,0.2]}
+            azimuth={[-1,0.75]}
+            config={{mass:2, tension:400}}
+            snap={{mass:4, tension:400}}
+            >
+            <Float 
+                rotationIntensity={0.4}>
+                        <rectAreaLight 
+                        width={2.5}
+                        height={1.65}
+                        intensity={55}
+                        color="#3399ff"
+                        rotation={[-0.1,Math.PI,0]}
+                        position={[0,0.55,-1.15]}
+                    />
+                <primitive
+                    object={computer.scene}
+                    position-y={-1.2}
+                >
+                    <Html
+                     transform 
+                     wrapperClass='htmlScreen'
+                     distanceFactor={1.17} 
+                     position={[0,1.56,-1.4]}
+                     rotation-x={-0.256}>
+                        <iframe src='https://manojkumar2105.github.io/THREEJS-Portfolio' />
+                    </Html>
+                </primitive>
+              
+            </Float>
+        </PresentationControls>
+        <Float>
+        <Text
+                    font='./bangers-v20-latin-regular.woff'    
+                    fontSize={0.75}
+                    position={[2,0.75,0.75]}           
+                    color="gold"
+                    rotation-y={-1.25}
+                    maxWidth={2}
+               >
+                    MANOJ KUMAR
+               </Text>
+               </Float>
+    </>
+}
